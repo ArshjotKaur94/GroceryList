310787d52d8b484dcb1aa434058a8f5b8f7cf894
ArshjotKaur94 <arshjotkaur70@gmail.com>
2020-12-04 13:58:17 -0500

    [Arshjot] contact page design

--- app/src/main/java/com/example/project_android/UserModel.java
+++ app/src/main/java/com/example/project_android/UserModel.java
@@ -3,9 +3,42 @@ package com.example.project_android;
 import io.realm.RealmObject;
 
 public class UserModel extends RealmObject {
-
+    private String firstName="";
+    private String lastName="";
+    private String phoneNumber="";
     private String userName = "";
     private String password = "";
+    private String email="";
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
 
     public void setUserName(String userName)
     {
@@ -26,5 +59,13 @@ public class UserModel extends RealmObject {
     public String getPassword()
     {
         return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
